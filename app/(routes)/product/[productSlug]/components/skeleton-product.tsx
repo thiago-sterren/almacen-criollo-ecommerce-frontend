adb96d3f4f44e1e53f0155f55ddb154c0f8ce32f
thiago-sterren <thiagosterren@gmail.com>
2025-09-14 18:56:33 -0300

    changing skeleton product

--- app/(routes)/product/[productSlug]/components/skeleton-product.tsx
+++ app/(routes)/product/[productSlug]/components/skeleton-product.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 const SkeletonProduct = () => {
     return (
-        <div className="grid sm:grid-cols-2 sm:py-16 sm:pr-40 sm:pl-180 pl-5">
+        <div className="grid sm:grid-cols-2 sm:py-16 sm:pr-40 sm:pl-100 pl-5">
             <Skeleton className="h-[200px] w-[350px] rounded-xl" />
             <div className="space-y-2">
                 <Skeleton className="h-8 w-[250px]" />
